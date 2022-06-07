b2a2704552f91a29f66bd224ceb95cad11162415
guilherme-taira <54184559+guilherme-taira@users.noreply.github.com>
2022-06-06 23:06:11 -0300

    teste sem as esclamações no scr da imagem

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -4,10 +4,10 @@
         @foreach ($viewData['products'] as $product)
             <div class="col-md-4 col-lg-3 mb-2">
                 <div class="card">
-                    <img src="{!!Storage::disk('s3')->url('produtos/'.$product->getId().'/'.$product->getImage())!!}" class="card-img-top img-card">
+                    <img src="{{Storage::disk('s3')->url('produtos/'.$product->getId().'/'.$product->getImage())}}" class="card-img-top img-card">
                     <div class="card-body text-center">
-                        <a href="{{ route('products.show', ['id' => $product['id']]) }}"
-                            class="btn bg-primary text-white">{{ $product['name'] }}</a>
+                        <a href="{{ route('products.show', ['id' => $product->getId()]) }}"
+                            class="btn bg-primary text-white">{{ $product->getName() }}</a>
                     </div>
                 </div>
             </div>
