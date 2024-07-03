5170c5b1d3ad1e3daf825e1ad29da1f12e7b1660
guilherme-taira <54184559+guilherme-taira@users.noreply.github.com>
2024-07-03 17:09:17 -0300

    remove a abstract class

--- app/Http/Controllers/MercadoPago/Pagamento/MercadoPagoPreference.php
+++ app/Http/Controllers/MercadoPago/Pagamento/MercadoPagoPreference.php
@@ -2,14 +2,94 @@
 
 namespace App\Http\Controllers\MercadoPago\Pagamento;
 
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\MercadoLivre\RefreshTokenController;
 use App\Models\token;
 use DateTime;
 use Illuminate\Support\Facades\Log;
 
-class MercadoPagoPreference extends AbstractMercadoPagoPreference
+class MercadoPagoPreference extends Controller
 
 {
+
+    private $externalPreference;
+    private $itens;
+    private $notificationUrl;
+    private $userid;
+
+
+    public function __construct(MercadoPagoCesta $itens,$notificationUrl,$userid)
+    {
+
+        $this->externalPreference = uniqid('afilidrop');
+        $this->itens = $itens;
+        $this->notificationUrl = $notificationUrl;
+        $this->userid = $userid;
+    }
+
+      /**
+     * Get the value of externalPreference
+     */
+    public function getExternalPreference()
+    {
+        return $this->externalPreference;
+    }
+
+    /**
+     * Set the value of externalPreference
+     */
+    public function setExternalPreference($externalPreference): self
+    {
+        $this->externalPreference = $externalPreference;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of itens
+     */
+    public function getItens()
+    {
+        return $this->itens;
+    }
+
+    /**
+     * Set the value of itens
+     */
+    public function setItens($itens): self
+    {
+        $this->itens = $itens;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of notificationUrl
+     */
+    public function getNotificationUrl()
+    {
+        return $this->notificationUrl;
+    }
+
+    /**
+     * Set the value of notificationUrl
+     */
+    public function setNotificationUrl($notificationUrl): self
+    {
+        $this->notificationUrl = $notificationUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userid
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+
     const URL_BASE  = "https://api.mercadopago.com/";
 
     public function toJson(){
