30021115df34609d78502d88f621b68de38a38b9
guilherme-taira <54184559+guilherme-taira@users.noreply.github.com>
2024-07-25 13:55:44 -0300

    adiciona na rota de notificacao logs

--- resources/views/shopify/create.blade.php
+++ resources/views/shopify/create.blade.php
@@ -54,6 +54,29 @@
                         required>
                 </div>
 
+                <hr>
+                <div class="alert alert-warning d-flex align-items-center mt-4" role="alert">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" width = "16px" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </svg>
+                    <div>
+                         Atenção: Os campos default só serão preenchido em caso de não ter esses dados dentro do pedido!
+                    </div>
+                  </div>
+
+                <div class="form-group col-md-6">
+                    <label for="apiKey" class="form-label">Email - Default</label>
+                    <input type="text" class="form-control" id="email" name="email"
+                        value="{{ isset($viewData['shopify']->email) ? $viewData['shopify']->email : '' }}" required>
+                </div>
+
+                <div class="form-group col-md-6 mt-2">
+                    <label for="apiKey" class="form-label">Telefone - Default</label>
+                    <input type="text" class="form-control" id="telefone" name="telefone"
+                        value="{{ isset($viewData['shopify']->telefone) ? $viewData['shopify']->telefone : '' }}" required>
+                </div>
+
             </div>
             <button type="submit" class="btn btn-primary mt-3">Salvar</button>
         </form>
