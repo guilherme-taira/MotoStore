559b58731f06bcb2b089b7ce1d16660149d66ac7
guilherme-taira <54184559+guilherme-taira@users.noreply.github.com>
2024-08-26 17:26:41 -0300

    este commit cria a integração com o site

---
+++ resources/views/fretes/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title', $viewData['title'])
+@section('subtitle', $viewData['subtitle'])
+@section('conteudo')
+
+    <div class="container-fluid px-4">
+        <h2 class="mt-4">Vendas</h2>
+
+    </div>
+    {{-- AJAX JQUERY SEARCH --}}
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.theme.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
+
+
+    <script>
+        $(document).ready(function() {
+
+
+        });
+    </script>
+
+@endsection
